f939aa73fe93b541704a452d9a2aeb9ad2a50ca1
Abhinandan Saha <abhinandansaha2004@gmail.com>
2026-02-20 16:49:24 +0530

    Update tutorial_1.py
    Test Changes

--- tutorial_1.py
+++ tutorial_1.py
@@ -2,4 +2,10 @@ def func(a,b):
     sum = a + b
     return sum
 (a,b) = (5,3)
+
 print (func(a,b))
+
+def minimum(a, b):
+    return min(a, b)
+
+print("The minimum of the two numbers is", minimum(a,b))
